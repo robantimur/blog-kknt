4bc3107b02954719b43dcd8af62bbef3cac3b9eb
Muhammad Fuad Fakhruzzaki <fuadzaki00@gmail.com>
2025-07-17 15:01:52 +0000

    Error: Invalid src prop (https://xoyfvbzzxnpvednkctur.supabase.co/storag

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'xoyfvbzzxnpvednkctur.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
